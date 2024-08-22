041242a1452adda8b829a4c804f5f70d82eb61f9
Shaik Yaseen <hackercanhack69@gmail.com>
2024-08-22 23:01:10 +0530

    fixed css of profile page

--- Frontend/src/components/Profile/Profile.jsx
+++ Frontend/src/components/Profile/Profile.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import "./Profile.css"
-import IMG from "../../../public/IMG.jpg"
-import EditIcon from '@mui/icons-material/Edit';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import Navbar from '../Navbar/Navbar'
+import React from "react";
+import "./Profile.css";
+import IMG from "../../../public/IMG.jpg"; // Make sure the path is correct
+import EditIcon from "@mui/icons-material/Edit";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
+import Navbar from "../Navbar/Navbar";
+import Skill from "./Skill";
 
 function Profile() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="profile">
         <div className="box">
-          <div className='imgContent'>
-          <img className='img' src={IMG} alt="img" />
-         <div className='imgcontent'>         
-         <h3>Full Name</h3>
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-         </div>
+          <div className="boxContent">
+            <div className="imgContent">
+              <img className="img" src={IMG} alt="Profile" />
+              <div className="imgcontent">
+                <h3>Full Name</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+              </div>
+            </div>
+            <p>
+              <EditIcon className="editIcon" />
+            </p>
           </div>
-         
-          <p>
-            <EditIcon className='editIcon'/>
-          </p>
-          <p><EmailOutlinedIcon/></p>
-
+          <div className="emailLine">
+            <EmailOutlinedIcon className="emailIcon" />
+            Shaik@gmail.com
+          </div>
+          <div className="phoneLine">
+          <CallOutlinedIcon className="phoneIcon"/> + 999999999   
         </div>
-       
+        <div className="skills">
+         <span className="skillWord">Skills :</span> 
+         <span>
+          <Skill/>
+          </span> 
+        </div>
+        <div className="resumeDiv">Resume</div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
---
+++ Frontend/src/components/Profile/Skill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Profile.css"
+
+function Skill() {
+  return (
+    <span className='skillBadge'>
+        Java
+    </span>
+  )
+}
+
+export default Skill
