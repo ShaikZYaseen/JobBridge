78c21d9837210d5858b0c9920abbc03cb4e05bd1
Shaik Yaseen <hackercanhack69@gmail.com>
2024-08-25 22:11:06 +0530

    made some changes

--- Frontend/src/components/JobDescription/JobDescription.jsx
+++ Frontend/src/components/JobDescription/JobDescription.jsx
@@ -1,14 +1,115 @@
-import React from 'react'
-import "./JobDescription.css"
-import Navbar from '../Navbar/Navbar'
-
+import React from "react";
+import "./JobDescription.css";
 
 function JobDescription() {
   return (
     <>
-    <Navbar/>
+      <div className="description">
+        <div className="onee">
+          <div className="one">
+            <h1>Title</h1>
+            <p className="badges">
+              <span className="badge1">12 positions</span>
+              <span className="badge2">Part time</span>
+              <span className="badge3">24 LPA</span>
+            </p>
+          </div>
+          <div className="two">
+            <div>Already Applied</div>
+          </div>
+        </div>
+        <div className="twoo">
+          <h1>Job Description </h1>
+          <hr />
+          <p className="twoo-rolee">
+            <span className="boldLetter">Role :</span> Frontend developer
+          </p>
+          <p className="twoo-role">
+            <span className="boldLetter">Location :</span>Hyderabad
+          </p>
+          <p className="twoo-role">
+            <span className="boldLetter">Experience :</span> (0-2)years
+          </p>
+          <p className="twoo-role">
+            <span className="boldLetter">Salary :</span> (9-10)LPA
+          </p>
+          <p className="twoo-role">
+            <span className="boldLetter">Total applicants :</span> 20
+          </p>
+          <p className="twoo-role">
+            <span className="boldLetter">Posted Date :</span> 18-03-2024
+          </p>
+          <p className="twoo-role">
+            <div className="boldLetter">Description :</div>
+            <br />{" "}
+            <p className="desc">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+              nulla delectus eaque at sunt fugiat, animi deleniti corporis fuga,
+              cumque veniam molestias sequi cum velit ipsa ipsam eius molestiae
+              inventore assumenda voluptates? Ut quidem libero, quisquam error
+              reiciendis voluptatum aliquam nihil natus quibusdam earum optio
+              vel excepturi vitae facilis nostrum fugit quia a aut quasi ad
+              minus odio incidunt magnam. Libero, veritatis! Officiis quaerat
+              dolorum necessitatibus accusantium sed quam libero repellendus
+              commodi, illum ipsa voluptates excepturi corrupti aliquam
+              doloribus praesentium delectus ex temporibus veritatis harum nobis
+              deserunt? Veniam officiis quibusdam inventore voluptatibus error
+              deleniti dolorem ipsam quos, officia accusantium voluptas. Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              tenetur eveniet iste facere? Eligendi fugit quae porro
+              necessitatibus rerum sed corporis dolore, illum quas dolorem,
+              dicta qui tempora esse accusantium laboriosam ipsum. Corporis id
+              excepturi, dolor praesentium veniam dicta distinctio, nobis
+              voluptate minima ipsam odio doloremque ex magnam quis! Totam
+              corporis, quidem soluta accusamus animi beatae voluptas quos
+              provident officia cum, rerum quaerat praesentium fuga atque
+              facilis repudiandae ipsum ad. Exercitationem, maxime. Voluptatem
+              maiores odio repellendus veritatis atque suscipit consequatur,
+              unde iste temporibus, illo deserunt. Nesciunt omnis quaerat
+              commodi sapiente expedita. Provident dolores nam eaque! Voluptatem
+              nobis corrupti voluptas animi? Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Corrupti praesentium soluta
+              inventore incidunt repudiandae officia fugit accusantium nemo
+              adipisci porro consectetur sint tempora dicta aperiam quasi
+              quaerat eligendi sed, iste mollitia corporis necessitatibus.
+              Aliquid hic quis, maxime enim sed exercitationem quisquam alias
+              aspernatur, reprehenderit vitae possimus soluta velit officia, sit
+              qui veritatis ut tempora neque fugit pariatur distinctio vel rem
+              in reiciendis. Repellat natus explicabo officia labore voluptatum
+              a, assumenda magnam aperiam sequi, doloremque minima, animi
+              placeat libero. Vero, quis ex. Iste id voluptatem debitis, iure
+              libero in harum veritatis maiores nisi enim corporis rerum magnam
+              impedit qui illo voluptatibus. Nulla distinctio pariatur hic neque
+              accusantium eius autem. Magnam rem est quod debitis quibusdam
+              corrupti illo, accusamus itaque ullam nostrum architecto illum
+              animi exercitationem odio. Voluptatem impedit laboriosam aut,
+              similique debitis sunt officia eius eveniet, atque incidunt,
+              facilis qui delectus iste veniam! Necessitatibus soluta obcaecati
+              reiciendis ratione dolore consequuntur, blanditiis ipsam dicta,
+              sequi, animi similique totam facilis quis alias. Illum explicabo
+              dolor exercitationem debitis soluta. Et recusandae cumque neque
+              cum fuga perferendis, est optio nam a velit, cupiditate, quos
+              nemo. Tempora enim minima optio doloribus alias, architecto
+              ducimus, odit dolorem voluptatem nobis non tenetur at! Perferendis
+              delectus tenetur sint deserunt, molestiae maiores ut excepturi
+              neque doloribus dignissimos necessitatibus maxime enim incidunt
+              porro sunt accusamus quasi velit nesciunt, consequatur sed ex?
+              Sapiente consequuntur cum accusamus itaque exercitationem
+              reprehenderit sint vitae dolor? Laboriosam, mollitia voluptate id
+              quia laborum commodi unde iure fugit eligendi ratione illum
+              inventore sint rerum nobis repudiandae minus soluta repellendus
+              iste libero similique earum? Sed dicta necessitatibus excepturi
+              exercitationem eveniet culpa ut quis repellat cum. Ipsam, vero eos
+              voluptate incidunt earum, dolorem laboriosam iusto dolor, sapiente
+              quaerat fugit adipisci veniam illum perferendis sint expedita.
+              Dolorum exercitationem sed vero eligendi ad, beatae ullam fuga
+              sunt similique quis tenetur atque suscipit!
+            </p>
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default JobDescription
+export default JobDescription;
