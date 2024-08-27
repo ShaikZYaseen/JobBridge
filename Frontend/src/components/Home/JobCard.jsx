1ba3f55e7a8ec0df6ee5ef60363fb2cdfbfcd9be
Shaik Yaseen <hackercanhack69@gmail.com>
2024-08-27 19:35:17 +0530

    fixed css

--- Frontend/src/components/Home/JobCard.jsx
+++ Frontend/src/components/Home/JobCard.jsx
@@ -7,7 +7,7 @@ function jobCard() {
       <h1>Company Name</h1>
       <p className='country'>India</p>
       <h3>Job Title</h3>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur .</p>
       <p className='badges'><span className="badge1">12 positions</span><span className='badge2'>Part time</span><span className='badge3'>24 LPA</span></p>
     </div>
   )
